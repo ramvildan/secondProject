ef9e98b5205a3c9326786f457b194a7e93b09452
Ramil Vildanov <ramvildan.work@gmail.com>
2022-08-17 15:48:36 +0300

    Delete Mapper.java

--- src/main/java/ru/levelup/h1/Mapper.java
+++
@@ -1,8 +0,0 @@
-package ru.levelup.h1;
-
-public class Mapper {
-    public User map(String line){
-        String[] values = line.split(",");
-        return new User(values[0], values[1], Integer.parseInt(values[2]), Integer.parseInt(values[3]));
-    }
-}
